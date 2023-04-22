8eb448c57b3585653e552ea8734a88079af3d9d7
Halina Tulchii <halendeihalyna@gmail.com>
2023-04-22 08:40:22 +0300

    update

--- src/components/FriendsList/FriendListItem.js
+++ src/components/FriendsList/FriendListItem.js
@@ -6,7 +6,7 @@ export default function FriendListItem(friends) {
     
     return (
             <li className={css.item} key={id}>
-            <span className={css.status}>{isOnline}</span>
+            <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={css.name}>{name}</p>
             </li>
